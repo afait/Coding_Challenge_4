58a612b0917fdfb6b953914368d34240d98a977a
Annabelle Fait <imannaf8@gmail.com>
2025-02-01 23:56:05 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -5,3 +5,11 @@ if (purchaseAmount > 100) {
     purchaseAmount -= 50
 }; // Now gives a $50 discount if the purchaseAmount is greater than $100
 console.log(`Final amount after discount: ${purchaseAmount}`); // console log the purchase amount after discount using a template literal
+
+// Task 2 - Sales Report
+let sales = [100, 200, 150, 100, 400]; // Defined five sales amounts
+let totalSales = 0; // starts totalSales at 0
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i]
+}; // will continue to add the sales data until it includes all of them
+console.log(`Total Sales: ${totalSales}`); // logs totalSales amount after calculation
