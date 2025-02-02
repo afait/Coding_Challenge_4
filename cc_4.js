8756f7140c5e8b279ebc73835e33245c232c8e0f
Annabelle Fait <imannaf8@gmail.com>
2025-02-01 23:36:36 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+// Task 1 If Statements
+let purchaseAmount = 300
+console.log("purchaseAmount:", purchaseAmount); // set purchase amount to 50
+if (purchaseAmount > 100) {
+    purchaseAmount -= 50
+}; // Now gives a $50 discount if the purchaseAmount is greater than $100
+console.log(`Final amount after discount: ${purchaseAmount}`); // console log the purchase amount after discount using a template literal
